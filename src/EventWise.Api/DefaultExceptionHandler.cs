c965a6fe8b4e79154a1101075b2f18cee5a8a17f
Mikael Kyllönen <mikael@mikaelkyllonen.com>
2024-12-26 10:49:05 +0200

    Add default exception handler and problem details

---
+++ src/EventWise.Api/DefaultExceptionHandler.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventWise.Api;
+
+public sealed class DefaultExceptionHandler(ILogger<DefaultExceptionHandler> logger, IWebHostEnvironment environment)
+    : IExceptionHandler
+{
+    private readonly ILogger<DefaultExceptionHandler> _logger = logger;
+    private readonly IWebHostEnvironment _environment = environment;
+
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        _logger.LogError(exception, "Unhandled exception occured");
+
+        var problemDetails = new ProblemDetails
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1",
+            Title = "An unexpected error occured",
+            Detail = _environment.IsDevelopment() ? exception.Message : null,
+            Instance = $"{httpContext.Request.Method} {httpContext.Request.Path}"
+        };
+
+        httpContext.Response.StatusCode = problemDetails.Status.Value;
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
--- src/EventWise.Api/Program.cs
+++ src/EventWise.Api/Program.cs
@@ -1,6 +1,11 @@
+using EventWise.Api;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddExceptionHandler<DefaultExceptionHandler>();
+builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
@@ -8,6 +13,9 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseStatusCodePages();
+app.UseExceptionHandler();
 
 app.UseHttpsRedirection();
 
