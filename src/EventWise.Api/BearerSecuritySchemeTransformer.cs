289f4966981ec2073a4b591936b88cbc571a30d7
Mikael Kyllönen <mikael@mikaelkyllonen.com>
2024-12-28 14:16:10 +0200

    Refactor into smaller methods

--- src/EventWise.Api/BearerSecuritySchemeTransformer.cs
+++ src/EventWise.Api/BearerSecuritySchemeTransformer.cs
@@ -10,48 +10,59 @@ internal sealed class BearerSecuritySchemeTransformer(IAuthenticationSchemeProvi
         var authenticationSchemes = await authenticationSchemeProvider.GetAllSchemesAsync();
         if (authenticationSchemes.Any(authScheme => authScheme.Name == "Bearer"))
         {
-            var requirements = new Dictionary<string, OpenApiSecurityScheme>
+            AddBearerSecurityScheme(document);
+            var authorizedEndpoints = GetAuthorizedEndpoints(context);
+            AddBearerSecurityRequirementsToAuthorizedEndpoints(document, authorizedEndpoints);
+        }
+    }
+
+    private static void AddBearerSecurityScheme(OpenApiDocument document)
+    {
+        var requirements = new Dictionary<string, OpenApiSecurityScheme>
+        {
+            ["Bearer"] = new OpenApiSecurityScheme
             {
-                ["Bearer"] = new OpenApiSecurityScheme
-                {
-                    Type = SecuritySchemeType.Http,
-                    Scheme = "bearer",
-                    In = ParameterLocation.Header,
-                    BearerFormat = "Json Web Token"
-                }
-            };
-            document.Components ??= new OpenApiComponents();
-            document.Components.SecuritySchemes = requirements;
-
-            var authorizedEndpoints = context.DescriptionGroups
-                .SelectMany(descriptionGroup => descriptionGroup.Items)
-                .Where(item => item.ActionDescriptor.EndpointMetadata.OfType<AuthorizeAttribute>().Any())
-                .Select(x => x.RelativePath)
-                .ToHashSet();
-
-            var operations = document.Paths
-                .Where(path => authorizedEndpoints.Contains(path.Key.TrimStart('/')))
-                .SelectMany(path => path.Value.Operations)
-                .ToArray();
-
-            foreach (var operation in operations)
-            {
-                operation.Value.Security.Add(new OpenApiSecurityRequirement
-                {
-                    [
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Id = "Bearer",
-                                Type = ReferenceType.SecurityScheme
-                            },
-
-
-                        }
-                    ] = Array.Empty<string>()
-                });
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                In = ParameterLocation.Header,
+                BearerFormat = "Json Web Token"
             }
+        };
+        document.Components ??= new OpenApiComponents();
+        document.Components.SecuritySchemes = requirements;
+    }
+
+    private static HashSet<string?> GetAuthorizedEndpoints(OpenApiDocumentTransformerContext context)
+    {
+        return context.DescriptionGroups
+            .SelectMany(descriptionGroup => descriptionGroup.Items)
+            .Where(item => item.ActionDescriptor.EndpointMetadata.OfType<AuthorizeAttribute>().Any())
+            .Select(x => x.RelativePath)
+            .ToHashSet();
+    }
+
+    private static void AddBearerSecurityRequirementsToAuthorizedEndpoints(OpenApiDocument document, HashSet<string?> authorizedEndpoints)
+    {
+        var operations = document.Paths
+            .Where(path => authorizedEndpoints.Contains(path.Key.TrimStart('/')))
+            .SelectMany(path => path.Value.Operations)
+            .ToArray();
+
+        foreach (var operation in operations)
+        {
+            operation.Value.Security.Add(new OpenApiSecurityRequirement
+            {
+                [
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Id = "Bearer",
+                            Type = ReferenceType.SecurityScheme
+                        }
+                    }
+                ] = Array.Empty<string>()
+            });
         }
     }
 }
