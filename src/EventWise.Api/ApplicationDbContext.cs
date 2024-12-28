43eb0de3200654ec152c753f0f06c516a28fccbc
Mikael Kyllönen <mikael@mikaelkyllonen.com>
2024-12-28 22:33:34 +0200

    Add ApplicationDbContext and EntityConfiguration for events

--- src/EventWise.Api/ApplicationDbContext.cs
+++ src/EventWise.Api/ApplicationDbContext.cs
@@ -1,10 +1,14 @@
 ﻿namespace EventWise.Api;
 
+using EventWise.Api.Events;
+
 using Microsoft.EntityFrameworkCore;
 
 public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
     : DbContext(options)
 {
+    public DbSet<BaseEvent> Events { get; set; } = default!;
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
