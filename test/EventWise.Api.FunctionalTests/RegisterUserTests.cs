4764ea1017cb3683519b13f33be0dced957d5af2
Mikael Kyllönen <mikael@mikaelkyllonen.com>
2024-12-29 21:34:53 +0200

    Add TUnit and WebAppFactory

---
+++ test/EventWise.Api.FunctionalTests/RegisterUserTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventWise.Api.FunctionalTests;
+internal class RegisterUserTests
+{
+}
--- test/EventWise.Api.FunctionalTests/WebAppFactory.cs
+++ test/EventWise.Api.FunctionalTests/WebAppFactory.cs
@@ -1,7 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using System.Net.Http.Json;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+using TUnit.Core.Interfaces;
 
 namespace EventWise.Api.FunctionalTests;
 
-public sealed class WebAppFactory : WebApplicationFactory
+public sealed class WebAppFactory : WebApplicationFactory<Program>, IAsyncInitializer
 {
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.RemoveAll(typeof(DbContextOptions<ApplicationDbContext>));
+
+            // Add db context for testing
+        });
+    }
+    public async Task InitializeAsync()
+    {
+        await InitializeTestUserAsync();
+    }
+
+    private async Task InitializeTestUserAsync()
+    {
+        var client = CreateClient();
+
+        await client.PostAsJsonAsync("users", new { Id = Guid.CreateVersion7(), FirstName = "Test", LastName = "User", Email = "test@localhost" });
+    }
 }
