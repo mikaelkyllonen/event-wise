ed189bc507645b94a7020d46f15b9d7af4ed3d70
Mikael Kyllönen <mikael@mikaelkyllonen.com>
2024-12-29 12:20:21 +0200

    Move User and UserError to own classes

---
+++ src/EventWise.Api/Events/User.cs
@@ -0,0 +1,40 @@
+﻿using EventWise.Api.Common;
+
+namespace EventWise.Api.Events;
+
+public sealed class User
+{
+    public Guid Id { get; private set; }
+    public string FirstName { get; private set; } = default!;
+    public string LastName { get; private set; } = default!;
+    public string Email { get; private set; } = default!;
+
+    public ICollection<EventParticipant> Participations { get; init; } = [];
+
+    private User()
+    { }
+
+    public static Result<User> Create(Guid id, string firstName, string lastName, string email)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+        {
+            return Result.Failure<User>(UserErrors.FirstNameRequired);
+        }
+        if (string.IsNullOrWhiteSpace(lastName))
+        {
+            return Result.Failure<User>(UserErrors.LastNameRequired);
+        }
+        if (string.IsNullOrWhiteSpace(email))
+        {
+            return Result.Failure<User>(UserErrors.EmailRequired);
+        }
+
+        return new User
+        {
+            Id = id,
+            FirstName = firstName,
+            LastName = lastName,
+            Email = email
+        };
+    }
+}
---
+++ src/EventWise.Api/Events/UserErrors.cs
@@ -0,0 +1,18 @@
+﻿using EventWise.Api.Common;
+
+namespace EventWise.Api.Events;
+
+public static class UserErrors
+{
+    public static readonly Error FirstNameRequired = new(
+        "User.FirstNameRequired",
+        "First name is required");
+
+    public static readonly Error LastNameRequired = new(
+        "User.LastNameRequired",
+        "Last name is required");
+
+    public static readonly Error EmailRequired = new(
+        "User.EmailRequired",
+        "Email is required");
+}
