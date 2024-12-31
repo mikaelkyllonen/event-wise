60e81f4bd0e94f5d0385384ae6697c71d3c4f0d1
Mikael Kyllönen <mikael@mikaelkyllonen.com>
2024-12-31 09:13:09 +0200

    Empty list test red

---
+++ test/EventWise.Api.FunctionalTests/Events/GetEventsTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http.Json;
+
+using EventWise.Api.FunctionalTests.Infrastructure;
+
+namespace EventWise.Api.FunctionalTests.Events;
+public sealed class GetEventsTests(WebAppFactory factory) : BaseFunctionalTests(factory)
+{
+    [Fact]
+    public async Task Returns_empty_list_when_no_events()
+    {
+        // Act
+        var response = await UserClient.GetAsync("events");
+     
+        // Assert
+        var events = await response.Content.ReadFromJsonAsync<GetEventsResponse>();
+        Assert.Empty(events);
+    }
+}
