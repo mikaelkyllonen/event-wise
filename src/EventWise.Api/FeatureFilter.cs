13d80f54113c05b56ed727d6ab4564a44f0475e0
Mikael Kyllönen <mikael@mikaelkyllonen.com>
2025-01-01 21:55:22 +0200

    Add feature filter

---
+++ src/EventWise.Api/FeatureFilter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.FeatureManagement;
+
+namespace EventWise.Api;
+
+public abstract class FeatureFilter(IFeatureManager featureManager) : IEndpointFilter
+{
+    private readonly IFeatureManager _featureManager = featureManager;
+
+    public abstract string FeatureFlag { get; }
+
+    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+        var isEnabled = await _featureManager.IsEnabledAsync(FeatureFlag);
+        
+        return !isEnabled 
+            ? Results.NotFound() 
+            : await next(context);
+    }
+}
