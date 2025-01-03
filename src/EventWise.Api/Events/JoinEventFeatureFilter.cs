55f094a7551e0063f7bf037b5d39348350d4fe87
dmiky-xamk <dmiky001@edu.xamk.fi>
2025-01-03 18:14:53 +0200

    Add join event point feature filter

---
+++ src/EventWise.Api/Events/JoinEventFeatureFilter.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.FeatureManagement;
+
+namespace EventWise.Api.Events;
+
+public class JoinEventFeatureFilter(IFeatureManager featureManager)
+    : FeatureFilter(featureManager)
+{
+    public override string FeatureFlag => "JoinEvent";
+}
