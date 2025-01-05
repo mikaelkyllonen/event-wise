09a9875d4386f0b62e2545583437d13a11d43612
dmiky-xamk <dmiky001@edu.xamk.fi>
2025-01-05 17:19:25 +0200

    Break into smaller methods

--- src/EventWise.Api/Events/EventStateTransitionBackgroundService.cs
+++ src/EventWise.Api/Events/EventStateTransitionBackgroundService.cs
@@ -12,32 +12,40 @@ public sealed class EventStateTransitionBackgroundService(IServiceProvider servi
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            var currentUtcTime = DateTime.UtcNow;
-
             using var scope = _serviceProvider.CreateScope();
             var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            var currentUtcTime = DateTime.UtcNow;
 
-            var eventsToStart = await dbContext.Events
-                .Where(e => e.EventState == EventState.Published && e.StartTimeUtc <= currentUtcTime)
-                .ToListAsync(cancellationToken: stoppingToken);
-
-            foreach (var @event in eventsToStart)
-            {
-                @event.Start();
-            }
-
-            var eventsToComplete = await dbContext.Events
-                .Where(e => e.EventState == EventState.InProgress && e.EndTimeUtc <= currentUtcTime)
-                .ToListAsync(cancellationToken: stoppingToken);
-
-            foreach (var @event in eventsToComplete)
-            {
-                @event.Complete();
-            }
+            await StartEvents(currentUtcTime, dbContext, stoppingToken);
+            await CompleteEvents(currentUtcTime, dbContext, stoppingToken);
 
             await dbContext.SaveChangesAsync(stoppingToken);
 
             await Task.Delay(_checkInterval, stoppingToken);
+        }
+    }
+
+    private static async Task CompleteEvents(DateTime currentUtcTime, ApplicationDbContext dbContext, CancellationToken stoppingToken)
+    {
+        var eventsToComplete = await dbContext.Events
+                        .Where(e => e.EventState == EventState.InProgress && e.EndTimeUtc <= currentUtcTime)
+                        .ToListAsync(cancellationToken: stoppingToken);
+
+        foreach (var @event in eventsToComplete)
+        {
+            @event.Complete();
+        }
+    }
+
+    private static async Task StartEvents(DateTime currentUtcTime, ApplicationDbContext dbContext, CancellationToken stoppingToken)
+    {
+        var eventsToStart = await dbContext.Events
+                        .Where(e => e.EventState == EventState.Published && e.StartTimeUtc <= currentUtcTime)
+                        .ToListAsync(cancellationToken: stoppingToken);
+
+        foreach (var @event in eventsToStart)
+        {
+            @event.Start();
         }
     }
 }
